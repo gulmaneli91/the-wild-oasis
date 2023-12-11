db2e983648b22597598baccebbbeb7ef3389441e
Ailton Gulmaneli <ailton_gulmaneli@hotmail.com>
2023-12-10 22:11:11 -0300

    first commit architecture and styled components

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  background-color: var(--color-grey-300);
+  padding: 20px;
+`;
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1"> The Wild Oasis </Heading>
+            <div>
+              <Heading as="h2"> Check in and out </Heading>
+              <Button onClick={() => alert("Check in")}>
+                Check in
+              </Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Check out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3"> Form </Heading>
+            <form>
+              <Input type="number" placeholder="number of guests" />
+              <Input type="number" placeholder="number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
