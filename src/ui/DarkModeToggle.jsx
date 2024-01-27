3d9b806a41c32205177d833ff0147f113d8f32ff
Ailton Gulmaneli <ailton_gulmaneli@hotmail.com>
2024-01-26 23:07:45 -0300

    implements dark mode  and dashboard page and charts w/ recharts

---
+++ src/ui/DarkModeToggle.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useDarkMode } from "../context/DarkModeContext";
+import ButtonIcon from "./ButtonIcon";
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi2";
+
+function DarkModeToggle() {
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
+  return <ButtonIcon onClick={toggleDarkMode}>
+    {isDarkMode ? <HiOutlineSun />: <HiOutlineMoon />} </ButtonIcon>;
+}
+
+export default DarkModeToggle;
